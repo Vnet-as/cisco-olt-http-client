04d68d6a34d9f06a6d293194c6619033ccc6bf7f
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-29 00:08:21 +0100

    Initial work on Operation class

--- cisco_olt_http/client.py
+++ cisco_olt_http/client.py
@@ -5,11 +5,59 @@ from urllib.parse import urljoin
 
 LOGGER = logging.getLogger('cisco_olt_http.client')
 
+
+class Operation(object):
+    '''Base class for API operations'''
+
+    url = '/cgi-bin/xml-parser.cgi'
+    type = 'show'
+
+    def __init__(self, client):
+        self.client = client
+
+    def get_base_data(self):
+        return {
+            'request': {
+                'operation': {
+                    '@token': self.get_token(),
+                    '@type': self.get_type(),
+                }
+            }
+        }
+
+    def get_operation_data(self):
+        raise NotImplementedError()
+
+    def get_token(self):
+        return self.client.token
+
+    def get_type(self):
+        return self.type
+
+    def execute(self):
+        response = self.client._req(
+            url=self.url,
+            data=xmltodict.unparse(self.get_operation_data()))
+        return xmltodict.parse(response.content)
+
+
+class ShowEquipmentOp(Operation):
+
+    def get_operation_data(self, **data):
+        base_data = self.get_base_data()
+        op_data = {'@entity': 'equipment', 'equipment': {'@id': 0}}
+        op_data.update(data)
+        base_data['request']['operation'].update(op_data)
+        return base_data
+
+
 class Client(object):
 
     def __init__(self, base_url):
         self.base_url = base_url
         self.session = requests.Session()
+        # token is incremented before each operation
+        self.token = -1
 
     def login(self, username, password):
         login_data = {
@@ -18,6 +66,13 @@ class Client(object):
             'button': 'Login', 'textfield': 'UX_EQUIPNAME',
         }
         response = self._req('login.htm', data=login_data)
+        response.raise_for_status()
+        return response
+
+    def _op(self, op, incr_token=True):
+        if incr_token is True:
+            self.token += 1
+        return op.execute()
 
     def _req(self, url, **options):
         url = urljoin(self.base_url, url)
