e1aa9c5c80b5fccb56bdec557912590eba1b9e83
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-29 23:43:34 +0100

    Add 1st tests

---
+++ cisco_olt_http/tests/test_client.py
@@ -0,0 +1,18 @@
+
+from cisco_olt_http.client import Client
+
+
+class TestClientSimple:
+
+    @classmethod
+    def setup_class(cls):
+        cls.client = Client('http://base-url')
+
+    def test_token_inc(self):
+        t = self.client.token
+        assert self.client.token == t + 1
+
+    def test_login(self, mocker):
+        mocker.patch.object(self.client.session, 'post')
+        self.client.session.side_effect = mocker.Mock()
+        self.client.login('username', 'password')
