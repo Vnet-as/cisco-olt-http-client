ad1203b9b93d1be499698807e2307413c20bb573
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-31 18:42:35 +0100

    Add simple test for operation get_data

--- cisco_olt_http/tests/test_operations.py
+++ cisco_olt_http/tests/test_operations.py
@@ -1,2 +1,10 @@
 
 from cisco_olt_http import operations
+from cisco_olt_http.client import Client
+
+
+def test_get_data():
+    client = Client('http://base-url')
+    show_equipment_op = operations.ShowEquipmentOp(client)
+    op_data = show_equipment_op.get_data()
+    assert op_data
