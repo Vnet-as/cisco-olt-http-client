d6be06ad6225e9a3af021fb522f89d36ec8d6fb3
Michal Kuffa <michal.kuffa@gmail.com>
2017-01-01 23:10:23 +0100

    Add stupid test for operation result init

--- cisco_olt_http/tests/test_operations.py
+++ cisco_olt_http/tests/test_operations.py
@@ -1,6 +1,14 @@
 
+import os
+import pytest
 from cisco_olt_http import operations
 from cisco_olt_http.client import Client
+
+
+@pytest.fixture
+def data_dir():
+    return os.path.abspath(
+        os.path.join(os.path.dirname(__file__), 'data'))
 
 
 def test_get_data():
@@ -8,3 +16,19 @@ def test_get_data():
     show_equipment_op = operations.ShowEquipmentOp(client)
     op_data = show_equipment_op.get_data()
     assert op_data
+
+
+class TestOperationResult:
+
+    def test_ok_response(self, data_dir):
+
+        class Response:
+            pass
+        response = Response()
+
+        with open(os.path.join(data_dir, 'ok_response.xml')) as of:
+            response.content = of.read()
+
+        operation_result = operations.OperationResult(response)
+
+        assert not operation_result.error
